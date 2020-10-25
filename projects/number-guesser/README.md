262d830824c6f48faefb8af8249b54e66ed0d097
Anil Raj <anilraje122@gmail.com>
2020-10-25 11:28:03 +0530

    Add initial files for number-guesser game

---
+++ projects/number-guesser/README.md
@@ -0,0 +1,6 @@
+# Number Guesser
+## Game Rules :
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Player will be notified about guessess remaining
+- Player will be notified with correct answer if loose
---
+++ projects/number-guesser/app.js
@@ -0,0 +1,9 @@
+/* 
+# Number Guesser
+## Game Rules :
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guessess remaining
+- Notify player of correct answer if loose
+- Let player choose to play again
+*/
