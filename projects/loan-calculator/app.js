0348c1477f019d79895d0f3e44fc0dbe6e801cd1
Anil Raj <anilraje122@gmail.com>
2020-10-25 11:11:24 +0530

    Add final changes to loan-calc project

--- projects/loan-calculator/app.js
+++ projects/loan-calculator/app.js
@@ -5,6 +5,7 @@ calcResults = (e) => {
     const amount = document.querySelector('#amount');
     const interest = document.querySelector('#interest');
     const years = document.querySelector('#years');
+
     // Output
     const monthlyPayment = document.querySelector('#monthly-payment');
     const totalPayment = document.querySelector('#total-payment');
@@ -13,6 +14,7 @@ calcResults = (e) => {
     const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value) / 100 / 12;
     const calculaledPayments = parseFloat(years.value) * 12;
+
     // Compute monthly payment
     const x = Math.pow(1 + calculatedInterest, calculaledPayments);
     const monthly = (principal * x * calculatedInterest)/(x-1);
@@ -20,32 +22,55 @@ calcResults = (e) => {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculaledPayments).toFixed(2);
         totalInterest.value = ((monthly * calculaledPayments) - principal).toFixed(2);
+
+        // show result and hide loading
+        document.querySelector('#results').style.display = 'block';
+        document.querySelector('#loading').style.display = 'none';
     } else {
-        showError('Please check your number!');
+        showError('Error occured!');
     }
-    e.preventDefault();
 }
 
+/* Displa error message */
 showError = (err) => {
+    // hide result and loading
+    document.querySelector('#results').style.display = 'none';
+    document.querySelector('#loading').style.display = 'none';
+
     // Get elements
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
+
     // Create div
     const errorDiv = document.createElement('div');
+
     // Add class
     errorDiv.className = 'alert alert-danger';
+
     // Create text node
     errorDiv.appendChild(document.createTextNode(err));
+
     // Insert error above heading
     card.insertBefore(errorDiv, heading);
+
     // Clear error after 3 sec
     setTimeout(clearError, 3000);
 }
 
+/* Remove error message */
 clearError = () => {
     document.querySelector('.alert').remove();
 }
 
-
 /* Listen for submit */
-document.querySelector('#loan-form').addEventListener('submit', calcResults);
+document.querySelector('#loan-form').addEventListener('submit', (e) => {
+    // Hide results
+    document.querySelector('#results').style.display = 'none';
+
+    // Show loader
+    document.querySelector('#loading').style.display = 'block';
+    setTimeout(calcResults, 2000);
+
+    // Disable default behaviour (page refresh) of submit 
+    e.preventDefault();
+});
